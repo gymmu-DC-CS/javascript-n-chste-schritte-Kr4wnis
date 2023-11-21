a47bc690262ae382925ace6ae2e5b23a73dddaf6
Jimmy <e220859@edu.sbl.ch>
2023-11-21 09:58:27 +0100

    erste aufgabe gemacht

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+
+
 export function aufgabe01(args) {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement != "e"){
+    if (currentElement != "E")
     result.push(currentElement)
-  }
+    }
+}
+  return result.join("")
+}
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement != "e"){
+    if (currentElement != "E")
+    result.push(currentElement)
+    }
+}
   return result.join("")
 }
